c7659f4e53f2ae0b566580d9dff2e5b78208d43e
Sathish S. Karunanithi <Sathish.Karunanithi@TRAVELUP.LOCAL>
2020-05-13 10:07:25 +0100

    Add project files.

---
+++ Controllers/BarChartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using chartApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chartApplication.Controllers
+{
+    public class BarChartController : Controller
+    {
+        // GET: /<controller>/  
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult PopulationChart()
+        {
+            var populationList = PopulationDataAccessaLayer.GetCityPopulationList();
+            return Json(populationList);
+        }
+    }
+}
